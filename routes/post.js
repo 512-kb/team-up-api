8fd52c5710e90f250e966a4b2d4b70e0c92639b9
512-kb <kb98kb98kb@gmail.com>
2020-03-02 22:53:02 +0530

    disabled posts routes

--- routes/post.js
+++ routes/post.js
@@ -1,53 +1,53 @@
-const router = require("express").Router();
-const bodyParser = require("body-parser");
+// const router = require("express").Router();
+// const bodyParser = require("body-parser");
 const _ = require("lodash");
 const Post = require("../schema").Post;
 const Invitation = require("../schema").Invitation;
 
-router.use(bodyParser.json());
+// router.use(bodyParser.json());
 
-router
-  .route("/posts")
-  .get(async (req, res) => {
-    //{username,pgno,channel_id}
-    if (!req.query.username || !req.query.channel_id) {
-      res.send([]);
-      return;
-    }
-    const posts = await Post.find({ channel_id: req.query.channel_id })
-      .sort({ created: -1 })
-      .skip(req.query.page * 30)
-      .limit(30)
-      .sort({ created: 1 });
-    res.send(posts);
-  })
-  .post(async (req, res) => {
-    //{username,tags[],content,channel_id}
-    await new Post(_.assign(req.body, { created: Date.now() }))
-      .save()
-      .then(async post_obj => {
-        await Invitation.updateOne(
-          { channel_id: req.body.channel_id, username: req.body.username },
-          {
-            $inc: { post_count: 1 }
-          }
-        )
-          .then(() => res.send({ msg: "Post created", post: post_obj }))
-          .catch(err => {
-            if (err) res.send(err);
-          });
-      })
-      .catch(err => {
-        if (err) res.send(err);
-      });
-  })
-  .delete(async (req, res) => {
-    //{_id,username}
-    await Post.deleteOne(req.body, err => {
-      if (err) res.send({ msg: "Some error occured" });
-      else res.send({ msg: "Post Deleted" });
-    });
-  });
+// router
+//   .route("/posts")
+//   .get(async (req, res) => {
+//     //{username,pgno,channel_id}
+//     if (!req.query.username || !req.query.channel_id) {
+//       res.send([]);
+//       return;
+//     }
+//     const posts = await Post.find({ channel_id: req.query.channel_id })
+//       .sort({ created: -1 })
+//       .skip(req.query.page * 30)
+//       .limit(30)
+//       .sort({ created: 1 });
+//     res.send(posts);
+//   })
+//   .post(async (req, res) => {
+//     //{username,tags[],content,channel_id}
+//     await new Post(_.assign(req.body, { created: Date.now() }))
+//       .save()
+//       .then(async post_obj => {
+//         await Invitation.updateOne(
+//           { channel_id: req.body.channel_id, username: req.body.username },
+//           {
+//             $inc: { post_count: 1 }
+//           }
+//         )
+//           .then(() => res.send({ msg: "Post created", post: post_obj }))
+//           .catch(err => {
+//             if (err) res.send(err);
+//           });
+//       })
+//       .catch(err => {
+//         if (err) res.send(err);
+//       });
+//   })
+//   .delete(async (req, res) => {
+//     //{_id,username}
+//     await Post.deleteOne(req.body, err => {
+//       if (err) res.send({ msg: "Some error occured" });
+//       else res.send({ msg: "Post Deleted" });
+//     });
+//   });
 
 module.exports.getPosts = async query => {
   let res = [];
@@ -84,4 +84,4 @@ module.exports.savePost = async post_object => {
   return res;
 };
 
-module.exports.router = router;
+//module.exports.router = router;

dcd5da60391f4ec9d4cabde9a8317d2b372019ef
512-kb <kb98kb98kb@gmail.com>
2020-03-03 19:47:01 +0530

    post routes removed

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,5 @@ const router = require("express").Router();
 router.use(require("./user_auth"));
 router.use(require("./channel"));
 router.use(require("./top5"));
-//router.use(require("./post").router);
 
 module.exports = router;
--- routes/post.js
+++ routes/post.js
@@ -1,52 +1,6 @@
-// const router = require("express").Router();
-// const bodyParser = require("body-parser");
 const _ = require("lodash");
 const Post = require("../schema").Post;
 const Invitation = require("../schema").Invitation;
-
-// router.use(bodyParser.json());
-
-// router.route("/posts").get(async (req, res) => {
-//   //{username,pgno,channel_id}
-//   if (!req.query.username || !req.query.channel_id) {
-//     res.send([]);
-//     return;
-//   }
-//   const posts = await Post.find(_.omit({ channel_id: req.query.channel_id },"page"))
-//     .sort({
-//       created: -1
-//     })
-//     .skip(req.query.page * 20)
-//     .limit(20);
-//   res.send(posts.reverse());
-// });
-//   .post(async (req, res) => {
-//     //{username,tags[],content,channel_id}
-//     await new Post(_.assign(req.body, { created: Date.now() }))
-//       .save()
-//       .then(async post_obj => {
-//         await Invitation.updateOne(
-//           { channel_id: req.body.channel_id, username: req.body.username },
-//           {
-//             $inc: { post_count: 1 }
-//           }
-//         )
-//           .then(() => res.send({ msg: "Post created", post: post_obj }))
-//           .catch(err => {
-//             if (err) res.send(err);
-//           });
-//       })
-//       .catch(err => {
-//         if (err) res.send(err);
-//       });
-//   })
-//   .delete(async (req, res) => {
-//     //{_id,username}
-//     await Post.deleteOne(req.body, err => {
-//       if (err) res.send({ msg: "Some error occured" });
-//       else res.send({ msg: "Post Deleted" });
-//     });
-//   });
 
 module.exports.getPosts = async query => {
   let res = [];
@@ -81,5 +35,3 @@ module.exports.savePost = async post_object => {
     });
   return res;
 };
-
-//module.exports.router = router;

d094f75dcb326d1afed4acde97d7c5dd527a8de7
512-kb <kb98kb98kb@gmail.com>
2020-06-25 12:00:02 +0530

    validation

--- routes/user_auth.js
+++ routes/user_auth.js
@@ -6,7 +6,7 @@ const User = require("../schema").User;
 router.use(bodyParser.json());
 
 router.get("/login", async (req, res) => {
-  let user = await User.find(req.query, err => {
+  let user = await User.find(req.query, (err) => {
     if (err) res.send({ msg: "Invalid Credentials" });
   });
   if (user.length <= 0) res.send({ msg: "Invalid Credentials" });
@@ -18,13 +18,32 @@ router.get("/login", async (req, res) => {
     });
 });
 
-router.post("/register", async (req, res) => {
-  let user = await User.find({ username: req.body.username });
-  if (user.length > 0) {
-    res.send({ msg: "Username already taken" });
+router.get("/validate", async ({ query }, res) => {
+  let [user] = await User.find({
+    $or: [{ username: query.username }, { email: query.email }]
+  });
+  let obj = {};
+  if (user) {
+    if (user.email === query.email) obj.email = "E-mail already registered";
+    if (user.username === query.username)
+      obj.username = "Username already taken";
+  }
+  res.send(obj);
+});
+
+router.post("/register", async ({ body }, res) => {
+  let [user] = await User.find({
+    $or: [{ username: body.username }, { email: body.email }]
+  });
+  if (user) {
+    let obj = {};
+    if (user.email === body.email) obj.email = "E-mail already registered";
+    if (user.username === body.username)
+      obj.username = "Username already taken";
+    res.send(obj);
     return;
   }
-  user = new User(_.assign(req.body, { created: Date.now() }));
+  user = new User(_.assign(body, { created: Date.now() }));
   await user.save().then(({ _id, username, region }) => {
     res.send({ _id, username, region });
   });
